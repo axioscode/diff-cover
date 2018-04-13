1617b68ee57efaf91ecc81d62e5ae2ea55426498
Brian Jacobel <bjacobel@gmail.com>
2018-04-13 14:40:46 -0500

    Stray space

--- diff-cover.sh
+++ diff-cover.sh
@@ -3,4 +3,4 @@
 [ -z "$COVER_BASE" ] && COVER_BASE="master"
 FILES="$(git diff --name-only $COVER_BASE -- '*.js'|paste -s -d '|' -)"
 
- node_modules/.bin/jest --coverage --collectCoverageFrom=\"$FILES\"
+node_modules/.bin/jest --coverage --collectCoverageFrom=\"$FILES\"
